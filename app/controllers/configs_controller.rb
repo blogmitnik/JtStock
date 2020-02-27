dfb58a6df3d35466d810929dc865e5710df5d333
David Wang <David.Wang@quantatw.com>
2020-02-27 12:49:55 +0800

    update 20200227

--- app/controllers/configs_controller.rb
+++ app/controllers/configs_controller.rb
@@ -4,7 +4,7 @@ class ConfigsController < ApplicationController
   def index
   	page = (params[:page] || 1).to_i
   	@post = Post.find(params[:my_post_id])
-    @report = @post.mi_reports.find(params[:report_id])
+    @report = @post.reports.find(params[:report_id])
     @station = @report.station
     @reports_for_cart = @post.mi_reports.where('station_id = ? AND published_at = ?', @report.station_id, @report.published_at).order("published_at DESC")
   	@reports = @reports_for_cart.page(page)
